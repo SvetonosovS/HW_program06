b3698eb6cd98515ae170a1bb78b2892d09ad82e1
SvetonosovS <tris23@bk.ru>
2022-10-02 23:28:36 +0300

    even

---
+++ Program_6.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число: ");
+string userInput = Console.ReadLine() ?? "";
+int userNumber = int.Parse(userInput);
+
+if (userNumber%2 ==0)
+{
+    Console.WriteLine(userNumber + " -> да(четное)");
+}
+ else
+{
+    Console.WriteLine(userNumber + " -> нет(нечетное)");
+}
